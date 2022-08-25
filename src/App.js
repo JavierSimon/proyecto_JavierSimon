1779e20638b512656da33af0bcac7fecc656bbf8
Javier Simon <javier.jr.simon.91@gmail.com>
2022-08-24 21:06:39 -0300

    Se agregaron detalles para cumplir con el Desafío crea tu landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ItemListContainer } from "./components/Items/ItemListContainer";
+import NavBar from "./components/NavBar/NavBar";
+
 
 function App() {
+  const greeting = 'Hola a todos soy el greeting'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+
+      <ItemListContainer greeting={greeting} bg='lightblue' pd='5px' />
+      <ItemListContainer greeting='Soy otro greeting' bg='lightgreen' pd='5px' />
+    </>
   );
 }
 
---
+++ src/components/Items/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ItemListContainer = ({ greeting, bg, pd }) => {
+    return (
+        <>
+            <div style={{ backgroundColor: bg, padding: pd }}>
+                <h2>El mensaje Greeting es: {greeting}</h2>
+            </div>
+        </>
+    )
+}
